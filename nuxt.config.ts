da7c53386c73801b5d7adde57b381faf01b08c7a
minhNN03 <minhnnguyen003@gmail.com>
2026-05-05 21:25:43 +0700

    feat: robot and sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,28 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/sitemap', '@nuxtjs/robots'],
 
   // domain chính
+  site: {
+    url: 'https://hoanglinhdvkdns.com',
+    name: 'Công ty dịch vụ nông sản Hoàng Linh',
+  },
+
+  // cấu hình sitemap
   sitemap: {
-    siteUrl: 'https://hoanglinhdvkdns.com'
+    sources: [
+      // create our own API endpoints
+      '/api/__sitemap__/urls',
+      // use a static remote file
+      'https://cdn.example.com/my-urls.json',
+      // hit a remote API with credentials
+      ['https://api.example.com/pages/urls', { headers: { Authorization: 'Bearer <token>' } }]
+    ]
+  }
+
+
+  robots: {
+    blockNonSeoBots: true,
+    blockAiBots: true,
+
   },
 
   // giữ nguyên config của bạn
